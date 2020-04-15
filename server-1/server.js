35afa04a94102c280f9a0f655232eb8e1ae30140
Tomasz Martynski <tomek@martynski.pl>
2020-04-15 14:04:28 +0200

    add multi core

--- server-1/server.js
+++ server-1/server.js
@@ -1,9 +1,26 @@
+const cluster = require("cluster");
 const http = require("http");
+const numCPUs = require("os").cpus().length;
 
-let i = 0;
-http
-  .createServer((_, res) => {
-    res.write(`Hello world(http): ${i++}`);
-    return res.end();
-  })
-  .listen(80);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  let i = 0;
+  http
+    .createServer((_, res) => {
+      res.write(`Hello world(http): ${i++}`);
+      return res.end();
+    })
+    .listen(80);
+
+  console.log(`Worker ${process.pid} started`);
+}
--- server-2/server.js
+++ server-2/server.js
@@ -1,11 +1,19 @@
-const fastify = require("fastify")({
-  logger: false,
-});
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const fastify = require("fastify")();
 
-let i = 0;
-fastify.get("/", (request, reply) => {
-  reply.send(`Hello world (fastify): ${i++}`);
-});
-
-// Run the server!
-fastify.listen(80);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  let i = 0;
+  fastify.get("*", (req, res) => {
+    res.send(`Hello world(fastify): ${i++}`);
+  });
+  fastify.listen(80, "0.0.0.0");
+}
