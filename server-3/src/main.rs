84327ebe10bc2d47a73739849f130dd67094c29d
Tomasz Martynski <tomek@martynski.pl>
2020-04-15 17:13:59 +0200

    dasda

---
+++ server-3/src/main.rs
@@ -0,0 +1,18 @@
+use actix_web::{web, App, HttpRequest, HttpServer};
+
+async fn index(req: HttpRequest) -> &'static str {
+    "Hello world!"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").to(index))
+    })
+    .backlog(1024)
+    .bind("0.0.0.0:3003")?
+    .run()
+    .await
+}
